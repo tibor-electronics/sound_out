029ba510932d6b4f0a97519da39e055d8b653c65
Kevin Lindsey <kevin@kevlindev.com>
2011-03-26 22:08:17 -0500

    Create separate wave generators for each audio channel. Wire up left and right channels. Lower the amplitude of the generated waves. Note that the waves are purposely de-tuned to create a beat effect

--- sound_out.vhd
+++ sound_out.vhd
@@ -9,7 +9,7 @@ use ieee.std_logic_unsigned.all;
 entity SoundOut is
 	port(
 		clock: in std_logic;
-      analog_out: out std_logic
+      audio_left, audio_right: out std_logic
 	);
 end SoundOut;
 
@@ -33,9 +33,12 @@ architecture behavioral of SoundOut is
 		);
 	end component;
 	signal tick: std_logic := '0';
-	signal wave: std_logic_vector(7 downto 0) := "01000000";
+	signal wave: std_logic_vector(7 downto 0) := "01110000";
+	signal tick2: std_logic := '0';
+	signal wave2: std_logic_vector(7 downto 0) := "01110000";
 begin
-	fundamental: Timer
+	-- left
+	fundamental_left: Timer
 		generic map(
 			TIMER_FREQUENCY => 880 -- 2x concert A
 		)
@@ -44,20 +47,48 @@ begin
 			reset => '0',
 			tick => tick
 		);
-	
-	sd: dac
+
+	sd_left: dac
 		port map(
 			clk => clock,
 			reset => '0',
 			DACin => wave,
-			DACout => analog_out
+			DACout => audio_left
 		);
-	
-	toggle: process(clock, tick)
+
+	toggle_left: process(clock, tick)
 	begin
 		if clock'event and clock = '1' then
 			if tick = '1' then
 				wave <= 255 - wave;
+			end if;
+		end if;
+	end process;
+
+	-- right
+	fundamental_right: Timer
+		generic map(
+			TIMER_FREQUENCY => 875
+		)
+		port map(
+			clock => clock,
+			reset => '0',
+			tick => tick2
+		);
+
+	sd_right: dac
+		port map(
+			clk => clock,
+			reset => '0',
+			DACin => wave2,
+			DACout => audio_right
+		);
+
+	toggle_right: process(clock, tick2)
+	begin
+		if clock'event and clock = '1' then
+			if tick2 = '1' then
+				wave2 <= 255 - wave2;
 			end if;
 		end if;
 	end process;
